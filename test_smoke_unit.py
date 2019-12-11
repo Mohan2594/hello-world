d680d418c780c3b96f13e6f57d47cbee021d3d83
Mohan2594 <54931737+Mohan2594@users.noreply.github.com>
2019-12-11 16:26:50 +0530

    smoketest Unittest framework

---
+++ test_smoke_unit.py
@@ -0,0 +1,23 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from unittestpackage.test_guestuser_case import GuestUser
+
+
+class TestCaseBrowser(unittest.TestCase):
+
+    #@classmethod
+    def unittestsmoke(self):
+        homePage = "https://store.aao.org/"
+        driver =webdriver.Chrome(executable_path="C:\\Users\\administrator1\\Downloads\\chromedriver_win32\\chromedriver.exe")
+        wait = WebDriverWait(driver, 10)
+        driver.maximize_window()
+        driver.get(homePage)
+        driver.implicitly_wait(5)
+        actions = ActionChains(driver)
+
+        GU =GuestUser(driver)
+        GU.guestflow()
+
+
